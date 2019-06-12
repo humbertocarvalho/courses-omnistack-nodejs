e74b938b5627c42b45afbf53bea6dc82a89af4dc
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-06-12 20:53:48 -0300

    Created get method.

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const uploadConfig = require('./config/upload');
 const routes = new express.Router();
 const upload = multer(uploadConfig);
 
+routes.get('/posts', PostController.index);
 routes.post('/posts', upload.single('image'), PostController.store);
 
 module.exports = routes;
