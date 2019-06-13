b8ba6a35f03afe7fe2117645afe1eaab5ea96982
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-06-12 21:12:16 -0300

    Created static configs to access images.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require('path');
+
 const app = express();
 
 dotenv.config();
 
 mongoose.connect(process.env.URI_DB, { useNewUrlParser: true });
 
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'uploads', 'resized'))
+);
 app.use(require('./routes'));
 
 app.listen(3333);
